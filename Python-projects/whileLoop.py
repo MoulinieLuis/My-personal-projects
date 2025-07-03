a9811d158136cb605a74821210eaf8f7929b7571
Moulinie Luis <161343288+MoulinieLuis@users.noreply.github.com>
2025-07-02 20:38:27 -0600

    For and While Loops programs added

---
+++ Python-projects/whileLoop.py
@@ -0,0 +1,11 @@
+"""
+This code aims to work with While Loops
+"""
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("Finished!")
+    
+    #The else block runs only if the loop wasn’t broken by break
